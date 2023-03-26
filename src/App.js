f7afdb94a1df85c8ab39e6c5e282f1e07a84706c
Aqirlone1 <aqirlone109@gmailcom>
2023-03-26 11:37:21 +0530

    added userlist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as  Switch, Route,Routes } from 'react-router-dom';
 import './App.css';
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Switch>
+      <Routes>
+       <Route path='/' element ={ <Users/>} exact/>
+       <Route path='/places/new' element ={ <NewPlace/>} exact />
+      </Routes>
+     </Switch>
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NewPlace = () => {
+return(
+    <h1> places !</h1>
+)
+}
+export default NewPlace
