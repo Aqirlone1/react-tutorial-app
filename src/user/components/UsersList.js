f51d37d9eb00eb99fe386c18ad7aaacdc7586631
Aqirlone1 <aqirlone109@gmailcom>
2023-03-26 13:06:08 +0530

    header

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,15 +1,17 @@
 import React from "react";
 import UserItem from "./UserItem.js";
 import './UsersList.css';
+import Card from "../../shared/components/UIElements/Card.js";
 
 const UsersList = props => {
   {
     if (props.items.length === 0)  {
-      return( <div className="center">
+      return(
+       <div className="center">
         <Card >
-                  <h2> No Users Found ! </h2>
-                   </Card>
-             </div>)
+           <h2> No Users Found ! </h2>
+        </Card>
+        </div>)
     } 
 
    return (
